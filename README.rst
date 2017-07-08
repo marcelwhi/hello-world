82b9c47e237b6e167740ea440da3b5b15091eb6e
Gregory Petukhov <lorien@lorien.name>
2017-07-09 00:12:46 +0300

    Migrate from YAML to JSON

--- README.rst
+++ README.rst
@@ -6,30 +6,30 @@ About project
 Coindatabase is a database of crypto currencies basic properties like market
 symbol, website, privacy/scalability/governor features, etc.
 
-Data of each currency are stored in a separate YAML file. Feel free to send
+Data of each currency are stored in a separate JSON file. Feel free to send
 pull requests to change existing coins or add new coins.
 
 
 How to add new coin
 -------------------
 
-Copy `coin_template.yml <coin_template.yml>`_ to coin/ directory and name it
+Copy `coin_template.json <coin_template.json>`_ to coin/ directory and name it
 with coin name. Use common coin name, do not lowercase all letters, use
 underscore instead of space.
 
 Good names:
 
-* Etherium_Classic.yml
-* NEM.yml
-* iDice.yml
+* Etherium_Classic.json
+* NEM.json
+* iDice.json
 
 Bad names:
 
-* Etherium Classic.yml
-* etherium_classic.yml
-* Nem.yml
-* nem.yml
-* idice.yml
+* Etherium Classic.json
+* etherium_classic.json
+* Nem.json
+* nem.json
+* idice.json
 
 Edit the new file and fill all coin propertes you know. If you do knot know or
 do not understand some properties just put "?" string there. Do not delete
@@ -73,6 +73,6 @@ If the value is unlimited use `Inf` literal e.g.:
 Coins
 -----
 
-* `Bitcoin <coin/Bitcoin.yml>`_
-* `Ethereum <coin/Ethereum.yml>`_
-* `Ripple <coin/Ripple.yml>`_
+* `Bitcoin <coin/Bitcoin.json>`_
+* `Ethereum <coin/Ethereum.json>`_
+* `Ripple <coin/Ripple.json>`_
--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 """
-To run tests you need to install pyyaml and pytest
+To run tests you need to install pytest
 """
 from glob import glob
 import os
 import re
-
-import yaml
+import json
 
 TEMPLATE_KEYS = [
     'name',
@@ -34,14 +33,14 @@ TEMPLATE_KEYS = [
 ]
 
 def test_coin_names():
-    re_coin_fname = re.compile(r'^[a-z0-9][a-z0-9_]+[a-z0-9]\.yml$', re.I)
+    re_coin_fname = re.compile(r'^[a-z0-9][a-z0-9_]+[a-z0-9]\.json$', re.I)
     for fname in os.listdir('coin'):
         if not re_coin_fname.match(fname):
             assert False, '%s is invalid coin filename' % fname
 
 
 def test_coin_template_keys():
-    tpl = yaml.load(open('coin_template.yml'))
+    tpl = json.load(open('coin_template.json'))
     assert set(tpl.keys()) == set(TEMPLATE_KEYS)
 
 
@@ -49,14 +48,14 @@ def test_coins_keys():
     re_extra_key = re.compile(
         '^(website|explorer|message_board)([2-9]|\d{2,})$'
     )
-    for fname in glob('coin/*.yml'):
-        coin = yaml.load(open(fname))
+    for fname in glob('coin/*.json'):
+        coin = json.load(open(fname))
         keys = [x for x in coin.keys() if not re_extra_key.match(x)]
         assert set(keys) == set(TEMPLATE_KEYS)
 
 
 def test_coins_readme_link():
-    for fname in glob('coin/*.yml'):
+    for fname in glob('coin/*.json'):
         name = fname.split('/')[1].split('.')[0]
         content = open('README.rst').read()
-        assert '<coin/%s.yml>' % name in content
+        assert '<coin/%s.json>' % name in content
