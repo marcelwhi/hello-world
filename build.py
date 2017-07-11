58ce45c063030fd9cd6f03201aae1e29a022d278
Gregory Petukhov <lorien@lorien.name>
2017-07-12 02:50:23 +0300

    New build

--- build.py
+++ build.py
@@ -18,7 +18,7 @@ CONST_KEYS = (
     'anonymous', 'core_lang',
     'references', 'lightning_network',
     'masternode', 'smart_contracts', 'lighning_network',
-    'blockchain_model',
+    'blockchain_model', 'announcement_url',
 )
 NUM_KEYS = (
     'max_supply', 'block_time', 'block_reward',
