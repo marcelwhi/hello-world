ff9785195562664ec1163e7859444e9e4e8fc019
Gregory Petukhov <lorien@lorien.name>
2017-07-17 00:23:07 +0300

    Add masternode & treasury

--- find_missing_data.py
+++ find_missing_data.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 from glob import glob
 import json
+import logging
 
 
 def main(**kwargs):
     for fname in glob('coin/*.json'):
-        coin = json.load(open(fname))
-        missing = []
-        for key, val in coin.items():
-            if val == "?" or val == []:
-                missing.append((key, val))
-        if missing:
-            print('File: %s' % fname)
-            for key, val in missing:
-                print(' * %s: %s' % (key, val))
+        try:
+            coin = json.load(open(fname))
+        except ValueError as ex:
+            logging.error('Error in %s' % fname, exc_info=ex)
+        else:
+            missing = []
+            for key, val in coin.items():
+                if val == "?" or val == []:
+                    missing.append((key, val))
+            if missing:
+                print('File: %s' % fname)
+                for key, val in missing:
+                    print(' * %s: %s' % (key, val))
 
 
 if __name__ == '__main__':
